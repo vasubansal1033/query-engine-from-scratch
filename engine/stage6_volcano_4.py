a4f9bc410f2a9808bc06fedf41a724daa7c158ae
Vasu Bansal <vasubansal1998@gmail.com>
2026-06-21 15:01:13 +0530

    completed stage6

--- engine/stage6_volcano_4.py
+++ engine/stage6_volcano_4.py
@@ -1,3 +1,4 @@
+import string
 from typing import Any, TYPE_CHECKING
 
 from sqloxide import parse_sql
@@ -7,10 +8,93 @@ if TYPE_CHECKING:
     from sqloxide import Expr
 
 
-def execute_expr(row: dict[str, Any], expr: "Expr") -> Any:
-    # TODO: copy from previous
-    ...
+def execute_binary_op(operation: str, left_operand: int, right_operand: int) -> int:
+    if operation == "Plus":
+        return left_operand + right_operand
+    elif operation == "Multiply":
+        return left_operand * right_operand
+    elif operation == "Minus":
+        return left_operand - right_operand
+    elif operation == "Divide":
+        if right_operand == 0:
+            raise Exception(f"division by zero: {left_operand} / {right_operand}")
+        return left_operand / right_operand
+    elif operation == "Gt":
+        if left_operand > right_operand:
+            return True
+        else:
+            return False
+    elif operation == "GtEq":
+        if left_operand >= right_operand:
+            return True
+        else:
+            return False
+    elif operation == "Lt":
+        if left_operand < right_operand:
+            return True
+        else:
+            return False
+    elif operation == "LtEq":
+        if left_operand <= right_operand:
+            return True
+        else:
+            return False
+    elif operation == "Eq":
+        if left_operand == right_operand:
+            return True
+        else:
+            return False
+    elif operation == "NotEq":
+        if left_operand != right_operand:
+            return True
+        else:
+            return False
+    else:
+        raise Exception(f"unknown binary op: {operation}")
 
+def parse_number_string(value: string):
+    try:
+        # try converting to int
+        return int(value)
+    except ValueError:
+        # if that fails, try converting to float
+        return float(value)
+
+def execute_expr(row: dict[str, Any], expr: "Expr") -> Any:
+    if "Value" in expr:
+        value = expr["Value"]["value"]
+        if "Number" in value:
+            value = value["Number"][0]
+            return parse_number_string(value)
+        elif "SingleQuotedString" in value:
+            # handle quoted string and number string
+            if type(value["SingleQuotedString"]) == list:
+                return value["SingleQuotedString"][0]
+            else:
+                return value["SingleQuotedString"]
+        elif "Boolean" in value:
+            return value["Boolean"]
+        elif "Null" in value:
+            return None
+    elif "Identifier" in expr:
+        column_name = expr["Identifier"]["value"]
+        return row[column_name]
+    elif "Nested" in expr:
+        nested = expr["Nested"]
+        return execute_expr(row, nested)
+    elif "BinaryOp" in expr:
+        binary_op = expr["BinaryOp"]
+        operation = binary_op["op"]
+
+        left_operand = execute_expr(row, binary_op["left"])
+        right_operand = execute_expr(row, binary_op["right"])
+        return execute_binary_op(
+            operation,
+            left_operand,
+            right_operand
+        )
+
+    raise Exception(f"unknown expr: {expr}")
 
 class Operator:
     def next(self) -> dict[str, Any] | None:
@@ -43,9 +127,18 @@ class Projection(Operator):
         self._exprs = exprs
 
     def next(self) -> dict[str, Any] | None:
-        # TODO
-        ...
+        maybe_row = self._child.next()
+        if not maybe_row:
+            return None
+        
+        result = {}
+        for key, expr in self._exprs.items():
 
+            result[key] = execute_expr(maybe_row, expr)
+        return result
+
+    def close(self):
+        self._child.close()
 
 class Filter(Operator):
     def __init__(self, expr: "Expr", child: Operator) -> None:
@@ -54,8 +147,14 @@ class Filter(Operator):
         self._expr = expr
 
     def next(self) -> dict[str, Any] | None:
-        # TODO
-        ...
+        maybe_row = self._child.next()
+        if not maybe_row:
+            return None
+
+        if execute_expr(maybe_row, self._expr) <= 0:
+            return self.next()
+
+        return maybe_row
 
 
 def build_plan(sql: str):
